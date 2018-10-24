8eec2e6ad8b1e6df95990713503f81695dd762f8
Hunter <chnhunter@aliyun.com>
2018-10-24 21:09:02 +0800

    !12 搭建 SpringBoot 项目框架 Merge pull request !12 from jimi_fa/f_INUFM_Jimi_20181024

---
+++ src/main/java/cn/edu/nefu/library/LibraryApplication.java
@@ -0,0 +1,17 @@
+package cn.edu.nefu.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : Jimi
+ * @date : 2018/10/24
+ * @since : Java 8
+ */
+@SpringBootApplication
+public class LibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryApplication.class, args);
+    }
+}
