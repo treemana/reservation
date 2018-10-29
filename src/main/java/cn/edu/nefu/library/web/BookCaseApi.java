86696c83994e2231f0133d1d0dd107d5935843e9
chenchenT <2277139615@qq.com>
2018-10-29 19:31:14 +0800

    !28 批量预留柜子 Merge pull request !28 from chenchenT/f_INVUD_chenchenT_20181028

--- src/main/java/cn/edu/nefu/library/web/BookCaseApi.java
+++ src/main/java/cn/edu/nefu/library/web/BookCaseApi.java
@@ -5,17 +5,16 @@ import cn.edu.nefu.library.common.LibException;
 import cn.edu.nefu.library.common.RestData;
 import cn.edu.nefu.library.common.util.JsonUtil;
 import cn.edu.nefu.library.common.util.TokenUtil;
+import cn.edu.nefu.library.core.model.BookCase;
 import cn.edu.nefu.library.core.model.User;
 import cn.edu.nefu.library.service.BookCaseService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -53,4 +52,27 @@ public class BookCaseApi {
             return new RestData(1, ErrorMessage.PLEASE_RELOGIN);
         }
     }
+
+    @RequestMapping(value = "/preorder", method = RequestMethod.POST)
+    public RestData setKeep(@RequestBody List<BookCase> bookCases, HttpServletRequest request) {
+
+        User currentUser = TokenUtil.getUserByToken(request);
+        int success = 0;
+        if (null != currentUser) {
+            for (BookCase bookCase : bookCases) {
+                if (0 >= bookCaseService.setKeepByNumber(bookCase)) {
+                    success = 1;
+                    break;
+                }
+            }
+            if (success == 0) {
+                return new RestData(0, "操作成功!");
+            } else {
+                return new RestData(1, "操作失败,请重试!");
+            }
+        } else{
+            return new RestData(1, ErrorMessage.PLEASE_RELOGIN);
+        }
+    }
+
 }
