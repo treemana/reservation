7cde126bdbfda22effc78aeb1af925e738791724
ZhengYi <chnhunter@aliyun.com>
2018-10-30 10:04:09 +0800

    规范代码

--- sql/libraryDML.sql
+++ sql/libraryDML.sql
@@ -1,1 +1,3 @@
-INSERT INTO `user` (user_username,user_password,user_type,user_token) VALUES ('admin','admin',1,NULL);
+-- Insert into library.user
+INSERT INTO `user` (`user_username`, `user_password`, `user_type`, `user_token`)
+VALUES ('admin', 'admin', 1, NULL);
