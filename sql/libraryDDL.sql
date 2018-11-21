f86141a75cc021772e25be23f9074631646c6304
Hunter <chnhunter@aliyun.com>
2018-11-21 17:38:21 +0800

    !105 add remote Merge pull request !105 from Hunter/f_add_remote_zhengyi_20181121

--- sql/libraryDDL.sql
+++ sql/libraryDDL.sql
@@ -24,7 +24,7 @@ DROP TABLE IF EXISTS `config`;
 CREATE TABLE `config` (
   `config_system_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT
   COMMENT '配置的id',
-  `config_key`       VARCHAR(10)              DEFAULT NULL
+  `config_key`       VARCHAR(15)              DEFAULT NULL
   COMMENT '配置的key',
   `config_value`     VARCHAR(255)             DEFAULT NULL
   COMMENT '配置的value（时间）',
