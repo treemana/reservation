f06c46d2521d4716a07ad9a963e2c3c2877a20a1
Hunter <chnhunter@aliyun.com>
2018-11-26 18:45:31 +0800

    add create db sql

--- sql/libraryDDL.sql
+++ sql/libraryDDL.sql
@@ -1,16 +1,25 @@
+-- Database `library`
+DROP DATABASE IF EXISTS `library`;
+CREATE DATABASE `library`
+  CHARACTER SET 'utf8'
+  COLLATE 'utf8_general_ci';
+
+USE `library`;
+
 -- Table `bookcase`
 DROP TABLE IF EXISTS `bookcase`;
-CREATE TABLE `bookcase` (
+CREATE TABLE `bookcase`
+(
   `bc_system_id` BIGINT UNSIGNED  NOT NULL AUTO_INCREMENT
-  COMMENT '表的id',
+    COMMENT '表的id',
   `bc_location`  TINYINT UNSIGNED NOT NULL
-  COMMENT '书柜的位置1，2，3，4代表不同的区域',
+    COMMENT '书柜的位置1，2，3，4代表不同的区域',
   `bc_number`    INT UNSIGNED     NOT NULL
-  COMMENT '书包柜的编号',
+    COMMENT '书包柜的编号',
   `bc_user_id`   INT UNSIGNED              DEFAULT NULL
-  COMMENT '书包柜对应的学生的id',
+    COMMENT '书包柜对应的学生的id',
   `bc_status`    TINYINT UNSIGNED NOT NULL DEFAULT '0'
-  COMMENT '书包柜的状态 默认0为可选',
+    COMMENT '书包柜的状态 默认0为可选',
   PRIMARY KEY (`bc_system_id`),
   UNIQUE KEY (`bc_number`),
   INDEX `idx_l` (`bc_location`),
@@ -21,13 +30,14 @@ CREATE TABLE `bookcase` (
 
 -- Table `config`
 DROP TABLE IF EXISTS `config`;
-CREATE TABLE `config` (
+CREATE TABLE `config`
+(
   `config_system_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT
-  COMMENT '配置的id',
-  `config_key`       VARCHAR(15)              DEFAULT NULL
-  COMMENT '配置的key',
-  `config_value`     VARCHAR(255)             DEFAULT NULL
-  COMMENT '配置的value（时间）',
+    COMMENT '配置的id',
+  `config_key`       VARCHAR(15)  DEFAULT NULL
+    COMMENT '配置的key',
+  `config_value`     VARCHAR(255) DEFAULT NULL
+    COMMENT '配置的value（时间）',
   PRIMARY KEY (`config_system_id`)
 )
   ENGINE = InnoDB
@@ -35,17 +45,18 @@ CREATE TABLE `config` (
 
 -- Table `user`
 DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
+CREATE TABLE `user`
+(
   `user_system_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT
-  COMMENT 'user表的id',
+    COMMENT 'user表的id',
   `user_username`  CHAR(10)        NOT NULL
-  COMMENT '学生端为学号，教师端为账号',
+    COMMENT '学生端为学号，教师端为账号',
   `user_password`  VARCHAR(10)     NOT NULL
-  COMMENT '密码',
-  `user_type`      TINYINT UNSIGNED         DEFAULT '0'
-  COMMENT '用户的状态，黑名单的同学为2，老师设置为1，学生设置为0，默认为0',
-  `user_token`     VARCHAR(255)             DEFAULT NULL
-  COMMENT '令牌',
+    COMMENT '密码',
+  `user_type`      TINYINT UNSIGNED DEFAULT '0'
+    COMMENT '用户的状态，黑名单的同学为2，老师设置为1，学生设置为0，默认为0',
+  `user_token`     VARCHAR(255)     DEFAULT NULL
+    COMMENT '令牌',
   PRIMARY KEY (`user_system_id`),
   UNIQUE KEY (`user_username`)
 )
--- src/main/resources/static/src/url.js
+++ src/main/resources/static/src/url.js
@@ -1,5 +1,9 @@
-//const req = "http://lib.nefu.edu.cn/";
-//const req = "http://138.68.54.208:8080/reservation/api/";
-const req = "http://222.27.227.102:8080/reservation/api/";
+/*
+ * Copyright (c) 2014-2018 www.itgardener.cn. All rights reserved.
+ */
+
+// const req = "http://lib.nefu.edu.cn/";
+// const req = "http://222.27.227.102:8080/reservation/api/";
 // const req = "http://222.27.227.19:9090/api/";
+const req = "http://39.96.21.23:8080/reservation/api/";
 export default req;
